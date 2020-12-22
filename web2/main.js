7226df903245c0b3497389cd68770399ce318021
duongtung76 <74946621+duongtung76@users.noreply.github.com>
2020-12-22 12:21:49 +0900

    Add files via upload

--- web2/main.js
+++ web2/main.js
@@ -1,1 +1,12 @@
 //Selectors
+let header = document.querySelector('.header');
+let humburgerMenu = document.querySelector('.hamburger-menu');
+
+window.addEventListener('scroll', function(){
+    let windowPosition = window.scrolly > 0;
+    header.classList.toggle('active',window.scrollY>0)
+})
+
+hamburgerMenu.addEventListener('click', function(){
+    header.classList.toggle('menu-open');
+})
